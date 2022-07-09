737e875ab34187d590d65eb9b4734ecdc7d9b768
Johan Bell <belljohan@hotmail.com>
2022-07-09 10:53:39 +0100

    Deploying with render

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: false,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    // "eslint:recommended",
+    //"plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
